e8c070b0c4c4d1127c616eb38cd25e485ec4449b
Lukas Gierth <lukas.gierth@posteo.de>
2025-06-16 15:27:25 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -26,16 +26,80 @@ dnf5 remove net-tools
 dnf5 remove vim
 # replaced by btop
 dnf5 remove htop
+
 # TODO:
 # basic tools
-dnf5 install tmux
+dnf5 install abcde
 dnf5 install android-tools
-dnf5 install gcc
+dnf5 install ansible
+dnf5 install aria2
+dnf5 install bat
+dnf5 install borgbackup
+dnf5 install borgmatic
+dnf5 install btop
+dnf5 install cmake
+dnf5 install ctags
+dnf5 install curl
+dnf5 install ddrescue
+dnf5 install detox
+dnf5 install dfu-utils
+dnf5 install distrobox
+dnf5 install duf
+dnf5 install gcc gcc-c++ glibc-static
+dnf5 install gdu
+dnf5 install esptool
+dnf5 install f3
+dnf5 install fastfetch
+dnf5 install fd-find
+dnf5 install ffmpeg
+dnf5 install fish
+dnf5 install fzf
+dnf5 install git
+dnf5 install gzip
+dnf5 install httpx
+dnf5 install ImageMagick
+dnf5 install iperf3
+dnf5 install luajit
+dnf5 install make
+dnf5 install mediainfo
+dnf5 install neovim
+dnf5 install nodejs
+dnf5 install pandoc
+dnf5 install picocom
+dnf5 install procs
+dnf5 install ripgrep
+dnf5 install rsync
+dnf5 install ruby
+dnf5 install shntool
+dnf5 install stow
+dnf5 install stress-ng
+dnf5 install subfinder
+dnf5 install tcpdump
+dnf5 install tealdeer
+dnf5 install unzip
+dnf5 install yt-dlp
 dnf5 install gnome-shell-extension-caffeine
-dnf5 install gcc gcc-c++ glibc-static 
 dnf5 install tailscale
+dnf5 install tmux
+dnf5 install wireshark
+dnf5 install whipper
+
+# dfu-programmer copr
+# eza copr
+# lazygit copr
+
+# mise
+#dnf5 install dnf-plugins-core
+dnf5 config-manager addrepo --from-repofile=https://mise.jdx.dev/rpm/mise.repo
+dnf5 install mise
 
 # TODO: docker
+dnf5 config-manager add-repo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+dnf5 install docker-ce
+dnf5 install docker-ce-cli
+dnf5 install containerd.io
+dnf5 install docker-buildx-plugin
+dnf5 install docker-compose-plugin
 
 # virt
 dnf5 group install --with-optional virtualization
@@ -47,9 +111,11 @@ dnf5 group install --with-optional virtualization
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
+# enable services
 systemctl enable podman.socket
 systemctl enable libvirtd.service
+systemctl enable docker.service
+
 # disable auto-updates (can be enabled again through ujust)
 systemctl disable flatpak-system-update.timer
 systemctl disable rpm-ostreed-automatic.timer
