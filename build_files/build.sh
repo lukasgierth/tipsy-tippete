c5f02475111f8e3afb8144f59afcc0ef2091aeed
Lukas Gierth <lukas.gierth@posteo.de>
2025-06-16 14:15:06 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,36 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# INFO: reference https://github.com/ublue-os/main/blob/main/packages.json
+# remove packages
+
+# no need for them
+dnf5 remove gnome-software
+dnf5 remove gnome-tour
+dnf5 remove gnome-tweaks
+dnf5 remove xorg-x11-xauth
+# installed with flatpak instead
+dnf5 remove firefox-langpacks
+dnf5 remove firefox
+# legacy net-tools
+dnf5 remove net-tools
+# replaced by neovim
+dnf5 remove vim
+# replaced by btop
+dnf5 remove htop
+# TODO:
+# basic tools
+dnf5 install tmux
+dnf5 install android-tools
+dnf5 install gcc
+dnf5 install gnome-shell-extension-caffeine
+dnf5 install gcc gcc-c++ glibc-static 
+dnf5 install tailscale
+
+# TODO: docker
+
+# virt
+dnf5 group install --with-optional virtualization
 
 # Use a COPR Example:
 #
@@ -20,5 +48,8 @@ dnf5 install -y tmux
 # dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
-
 systemctl enable podman.socket
+systemctl enable libvirtd.service
+# disable auto-updates (can be enabled again through ujust)
+systemctl disable flatpak-system-update.timer
+systemctl disable rpm-ostreed-automatic.timer
