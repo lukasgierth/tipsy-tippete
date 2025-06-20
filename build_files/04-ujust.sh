857f086c8bbbbb761521c2d4b117489d4be98841
Lukas Gierth <lukas.gierth@posteo.de>
2025-06-20 09:12:40 +0200

    feat: add task to create firewall zone for vnet interfaces

--- build_files/04-ujust.sh
+++ build_files/04-ujust.sh
@@ -6,7 +6,7 @@ set -ouex pipefail
 rm -f /usr/share/ublue-os/just/*.just
 
 # create own file
-cp -f /ctx/files/96-groups.just /usr/share/ublue-os/just/96-groups.just
+cp -f /ctx/files/96-virt.just /usr/share/ublue-os/just/96-virt.just
 cp -f /ctx/files/97-mise.just /usr/share/ublue-os/just/97-mise.just
 cp -f /ctx/files/98-flatpak.just /usr/share/ublue-os/just/98-flatpak.just
 cp -f /ctx/files/99-custom.just /usr/share/ublue-os/just/99-custom.just
@@ -15,7 +15,7 @@ cp -f /ctx/files/toolbox.ini /etc/toolbox/toolbox.ini
 
 # cleanup justfile imports
 sed -i '/^import/d' /usr/share/ublue-os/justfile
-echo 'import "/usr/share/ublue-os/just/96-groups.just"' >>/usr/share/ublue-os/justfile
+echo 'import "/usr/share/ublue-os/just/96-virt.just"' >>/usr/share/ublue-os/justfile
 echo 'import "/usr/share/ublue-os/just/97-mise.just"' >>/usr/share/ublue-os/justfile
 echo 'import "/usr/share/ublue-os/just/98-flatpak.just"' >>/usr/share/ublue-os/justfile
 echo 'import "/usr/share/ublue-os/just/99-custom.just"' >>/usr/share/ublue-os/justfile
