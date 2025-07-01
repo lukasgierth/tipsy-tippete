e8a49cc00e3201d9cac52f402787221d1c8aa8df
Lukas Gierth <lukas.gierth@posteo.de>
2025-07-01 21:44:24 +0200

    chore: revamp installs, add ublue-polkit-rules

--- build_files/02-install-packages.sh
+++ build_files/02-install-packages.sh
@@ -64,7 +64,26 @@ dnf5 -y install whipper
 dnf5 -y install wireshark
 dnf5 -y install yt-dlp
 
-# my own copr
+# virtualization
+dnf5 -y install libvirt-daemon-config-network
+dnf5 -y install libvirt-daemon-kvm
+dnf5 -y install qemu-kvm
+dnf5 -y install virt-install
+dnf5 -y install virt-manager
+
+# mise repo
+dnf5 -y config-manager addrepo --from-repofile=https://mise.jdx.dev/rpm/mise.repo
+dnf5 -y install mise
+
+# docker repo
+dnf5 -y config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+dnf5 -y install containerd.io
+dnf5 -y install docker-buildx-plugin
+dnf5 -y install docker-ce
+dnf5 -y install docker-ce-cli
+dnf5 -y install docker-compose-plugin
+
+# copr
 dnf5 -y copr enable gierth/tiny-tools
 dnf5 -y install ansel-bin
 dnf5 -y install blisp
@@ -75,28 +94,10 @@ dnf5 -y install lazygit
 dnf5 -y install rs-tftpd
 dnf5 -y copr disable gierth/tiny-tools
 
-# TODO: stuff from bluefin/aurora/bazzite in ublue copr?
+# ublue copr
 dnf5 -y copr enable ublue-os/packages
-dnf5 -y install ublue-os-libvirt-workarounds
 dnf5 -y install aurora-backgrounds
+dnf5 -y install ublue-os-libvirt-workarounds
+dnf5 -y install ublue-os-udev-rules # should be in silverblue-main already, just make sure
+dnf5 -y install ublue-polkit-rules
 dnf5 -y copr disable ublue-os/packages
-
-# mise
-dnf5 -y config-manager addrepo --from-repofile=https://mise.jdx.dev/rpm/mise.repo
-dnf5 -y install mise
-
-# docker
-dnf5 -y config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
-dnf5 -y install containerd.io
-dnf5 -y install docker-buildx-plugin
-dnf5 -y install docker-ce
-dnf5 -y install docker-ce-cli
-dnf5 -y install docker-compose-plugin
-# FIX: create systemd service that creates docker group and then disables itself permanently
-
-# virtualization
-dnf5 -y install libvirt-daemon-config-network
-dnf5 -y install libvirt-daemon-kvm
-dnf5 -y install qemu-kvm
-dnf5 -y install virt-install
-dnf5 -y install virt-manager
