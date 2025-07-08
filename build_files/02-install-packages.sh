8fed0c8e4096cb8237c96aadc25f3da55de85c1b
Lukas Gierth <lukas.gierth@posteo.de>
2025-07-08 16:35:56 +0200

    feat: add yazi, hex-rs

--- build_files/02-install-packages.sh
+++ build_files/02-install-packages.sh
@@ -90,11 +90,13 @@ dnf5 -y install blisp
 dnf5 -y install chezmoi
 dnf5 -y install dfu-programmer
 dnf5 -y install eza
+dnf5 -y install hex-rs
 dnf5 -y install hurl
 dnf5 -y install lazygit
 dnf5 -y install rs-tftpd
 dnf5 -y install systemctl-tui
 dnf5 -y install sysz
+dnf5 -y install yazi
 dnf5 -y copr disable gierth/tiny-tools
 
 # ublue copr
